c76b278d280f577e2c71b02407d3c3dbf3915311
bchevreau <b.chevreau@gmail.com>
2025-04-26 15:16:57 +0800

    testing

---
+++ components/xgimi_ble_advertiser/__init__.py
@@ -0,0 +1,13 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+
+xgimi_ble_advertiser_ns = cg.esphome_ns.namespace('xgimi_ble_advertiser')
+XgimiBleAdvertiser = xgimi_ble_advertiser_ns.class_('XgimiBleAdvertiser', cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(XgimiBleAdvertiser),
+}).extend(cv.COMPONENT_SCHEMA)
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
