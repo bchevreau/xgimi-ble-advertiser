04932ed0bcd366f15c5ac00bb3c3d75f946f937d
bchevreau <b.chevreau@gmail.com>
2025-04-26 16:40:07 +0800

    custom component

--- components/xgimi_ble_advertiser/__init__.py
+++ components/xgimi_ble_advertiser/__init__.py
@@ -1,5 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome import automation
+from esphome.const import CONF_ID
 
 xgimi_ble_advertiser_ns = cg.esphome_ns.namespace('xgimi_ble_advertiser')
 XgimiBleAdvertiser = xgimi_ble_advertiser_ns.class_('XgimiBleAdvertiser', cg.Component)
